195a8f75d3cf93d550c363d89b3e3f32499be23a
Kaelin Laundry <wasabifan@outlook.com>
2020-08-04 00:56:21 -0700

    Outline for data loading structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,40 @@ import React from "react";
 // @ts-ignore
 import COMBINATORS_DATA_IMPORT from "../../content/combinators.yaml";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
 interface Given {
-    type: string,
-    desc: string
+    type: string;
+    desc: string;
 }
 
-interface CombinatorData {
-    have: string,
-    want: string,
-    panics?: boolean,
-    candidates: string | string[],
-    given: Given[],
+interface Combinator {
+    have: string;
+    want: string;
+    panics?: boolean;
+    candidates: string | string[];
+    given: Given[];
 }
 
-const COMBINATORS_DATA: CombinatorData[] = COMBINATORS_DATA_IMPORT;
+interface AppData {
+    entries: Combinator[];
+}
+
+const APP_DATA: AppData = preprocessData(COMBINATORS_DATA_IMPORT);
+
+function preprocessData(importData: Combinator[]): AppData {
+    const entries = [...importData].sort(
+        (a, b) => a.have.localeCompare(b.have) || a.want.localeCompare(b.want)
+        // TODO: sort given and other fields
+    );
+
+    return { entries };
+}
 
 export default function Home() {
-    return <Layout>
-        <div>Data: {JSON.stringify(COMBINATORS_DATA)}</div>
-    </Layout>;
+    return (
+        <Layout>
+            <div>Data: {JSON.stringify(APP_DATA)}</div>
+        </Layout>
+    );
 }
