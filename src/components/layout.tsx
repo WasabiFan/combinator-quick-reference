1908e8e6ec5fa5195c05da73a49b3caac516eca2
Kaelin Laundry <wasabifan@outlook.com>
2020-08-04 00:55:52 -0700

    Remove unused template code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,28 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 
-// import Header from "./header"
 import SEO from "./seo";
 
 import "../../styles/normalize.css";
 import "../../styles/skeleton.css";
 
 const Layout = ({ children }: { children: any }) => {
-    const data = useStaticQuery(graphql`
-        query SiteTitleQuery {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `);
-
     return (
         <>
             <SEO />
-            {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
             <div className="section">
                 <div className="container">
                     <main>{children}</main>
