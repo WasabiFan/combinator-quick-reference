a141e2181ef694e34be38e0a449b319e4c7a5fa6
Kaelin Laundry <wasabifan@outlook.com>
2020-08-04 00:36:19 -0700

    Add typings shim for YAML import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-import COMBINATORS_DATA from "../../content/combinators.yaml";
+// @ts-ignore
+import COMBINATORS_DATA_IMPORT from "../../content/combinators.yaml";
 
 import Layout from '../components/layout'
+
+interface Given {
+    type: string,
+    desc: string
+}
+
+interface CombinatorData {
+    have: string,
+    want: string,
+    panics?: boolean,
+    candidates: string | string[],
+    given: Given[],
+}
+
+const COMBINATORS_DATA: CombinatorData[] = COMBINATORS_DATA_IMPORT;
 
 export default function Home() {
     return <Layout>
